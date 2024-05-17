47240b80d43c63342f4e874e928f348b9cd6235d
tumay ozyazici <tumay.mm@gmail.com>
2024-05-17 09:39:20 +0300

    a

--- Frontends/OnionCarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/OnionCarBook.WebUI/Controllers/ServiceController.cs
@@ -8,6 +8,8 @@ namespace OnionCarBook.WebUI.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.v1 = "Hizmetler";
+            ViewBag.v2 = "Hizmetlerimiz";
             return View();
         }
     }
